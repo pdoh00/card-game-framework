91a454ecdf35e180d7c7061d6e961791f29198f9
Phil <abc@abc.com>
2012-11-11 08:29:39 -0800

    Changed the signalRClientChannel to use a Global context getter
    I think that this could possibly be solved by configuration of autofac,
    but I am not sure how right now.

--- OFCP.Server/SignalRClientChannel.cs
+++ OFCP.Server/SignalRClientChannel.cs
@@ -3,70 +3,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Infrastructure;
+using OFCP.Server.Hubs;
+using SignalR;
 using SignalR.Hubs;
 
 namespace OFCP.Server
 {
     internal class SignalRClientChannel : IClientChannel
     {
-        private readonly IHubContext HubCtx;
         private readonly IPlayerConnectionMap _playerConnectionMap;
 
-        public SignalRClientChannel(IHubContext hubCtx, IPlayerConnectionMap playerConnectionMap)
+        public SignalRClientChannel(IPlayerConnectionMap playerConnectionMap)
         {
-            HubCtx = hubCtx;
             _playerConnectionMap = playerConnectionMap;
         }
         public void BroadcastPlayerSeated(string tableId, string name, int position)
         {
-            HubCtx.Clients[tableId].playerTookSeat(name, position, "");
+            GetPokerServerHubContext().Clients[tableId].playerTookSeat(name, position, "");
         }
 
         public void BroadcastPlayerLeft(string tableId, string name, int position)
         {
-            HubCtx.Clients[tableId].playerLeftSeat(name, position);
+            GetPokerServerHubContext().Clients[tableId].playerLeftSeat(name, position);
         }
 
         public void BroadcastPlayerHandSet(string tableId, int position)
         {
-            HubCtx.Clients[tableId].playerSetCards(position);
+            GetPokerServerHubContext().Clients[tableId].playerSetCards(position);
         }
 
         public void BroadcastPlayerCardsRearranged(string tableId, int playerPosition, bool[] cards)
         {
-            HubCtx.Clients[tableId].playerCardsRearranged(playerPosition, cards);
+            GetPokerServerHubContext().Clients[tableId].playerCardsRearranged(playerPosition, cards);
         }
 
         public void BroadcastDeckShuffled(string tableId)
         {
-            HubCtx.Clients[tableId].deckShuffled();
+            GetPokerServerHubContext().Clients[tableId].deckShuffled();
         }
 
         public void BroadcastGameStarted(string tableId, string gameId, DateTime timestamp)
         {
-            HubCtx.Clients[tableId].gameStarted(gameId, timestamp);
+            GetPokerServerHubContext().Clients[tableId].gameStarted(gameId, timestamp);
         }
 
         public void BroadcastPlayerReady(string tableId, int position)
         {
-            HubCtx.Clients[tableId].playerReadiedUp(position);
+            GetPokerServerHubContext().Clients[tableId].playerReadiedUp(position);
         }
 
         public void BroadcastMessage(string tableId, string message)
         {
-            HubCtx.Clients[tableId].broadcastToConsole(message);
+            GetPokerServerHubContext().Clients[tableId].broadcastToConsole(message);
         }
 
         public void PlayerDealtCards(string playerId, string[] cards)
         {
             var connectionId = _playerConnectionMap.GetConnectionIdForPlayer(playerId);
-            HubCtx.Clients[connectionId].dealCards(cards);
+            GetPokerServerHubContext().Clients[connectionId].dealCards(cards);
         }
 
         public void PlayerSeated(string playerId, int position)
         {
             var connectionId = _playerConnectionMap.GetConnectionIdForPlayer(playerId);
-            HubCtx.Clients[connectionId].playerRegistered(playerId, position);
+            GetPokerServerHubContext().Clients[connectionId].playerRegistered(playerId, position);
+        }
+
+        private static IHubContext GetPokerServerHubContext()
+        {
+            var ctx = GlobalHost.ConnectionManager.GetHubContext<PokerServer>();
+            return ctx;
         }
     }
 }
